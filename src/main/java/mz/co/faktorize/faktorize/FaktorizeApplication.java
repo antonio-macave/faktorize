4e1f024701b2bd6b00ed40793f1fce805a766efb
António Macave <antonio.macave@gmail.com>
2025-05-24 13:06:16 +0200

    Initial commit

---
+++ src/main/java/mz/co/faktorize/faktorize/FaktorizeApplication.java
@@ -0,0 +1,13 @@
+package mz.co.faktorize.faktorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaktorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaktorizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/co/faktorize/faktorize/FaktorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.faktorize.faktorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaktorizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
