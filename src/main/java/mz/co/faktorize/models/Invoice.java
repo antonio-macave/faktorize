f3574d91985105f2eb8564ba12e84d5d7ab10570
António Macave <antonio.macave@gmail.com>
2025-05-25 18:20:37 +0200

    Added a ManyToOne relationship with a Supplier

--- src/main/java/mz/co/faktorize/models/Invoice.java
+++ src/main/java/mz/co/faktorize/models/Invoice.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,5 +30,8 @@ public class Invoice {
     private double discountPercentage;
     private double discountAmount;
     private double netAmount;
+
+    @ManyToOne
+    private Supplier supplier;
 
 }
