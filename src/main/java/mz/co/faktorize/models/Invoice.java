d4c0ba68ac07601fd5b060e50e0c0b526a0e1149
António Macave <antonio.macave@gmail.com>
2025-05-28 11:28:42 +0200

    Added paid and items variables

--- src/main/java/mz/co/faktorize/models/Invoice.java
+++ src/main/java/mz/co/faktorize/models/Invoice.java
@@ -1,5 +1,8 @@
 package mz.co.faktorize.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,6 +35,10 @@ public class Invoice {
     private double discountPercentage;
     private double discountAmount;
     private double netAmount;
+    private boolean paid = false;
+
+    @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items;
 
     @ManyToOne
     @JoinColumn(name = "supplier_id")
