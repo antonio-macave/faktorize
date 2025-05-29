128824d825bf0cff4f2b1107c6d3be66478bf56b
António Macave <antonio.macave@gmail.com>
2025-05-29 14:07:14 +0200

    Added findInvoicesBySupplierId method

--- src/main/java/mz/co/faktorize/repository/InvoiceRepository.java
+++ src/main/java/mz/co/faktorize/repository/InvoiceRepository.java
@@ -1,5 +1,6 @@
 package mz.co.faktorize.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ import mz.co.faktorize.models.Invoice;
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     Optional<Invoice> findInvoiceById(Long id);
     Optional<Invoice> findInvoiceByNumber(String name);
+    Optional<List<Invoice>> findInvoicesBySupplierId(Long supplierId);
 }
