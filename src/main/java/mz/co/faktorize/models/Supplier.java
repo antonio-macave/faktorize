418433787216aa12c297f61e0579231d960e9901
António Macave <antonio.macave@gmail.com>
2025-05-29 14:06:34 +0200

    Adding city and country columns

--- src/main/java/mz/co/faktorize/models/Supplier.java
+++ src/main/java/mz/co/faktorize/models/Supplier.java
@@ -3,6 +3,7 @@ package mz.co.faktorize.models;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,9 +24,12 @@ public class Supplier {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String vatNumber;
     private String name;
     private String address;
+    private String city;
+    private String country;
 
     @OneToMany(mappedBy = "supplier", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Invoice> invoices;
