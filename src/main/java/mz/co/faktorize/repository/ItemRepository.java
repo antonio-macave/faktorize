0960bc611de5fb1020c70c14aa43c5d944f7cb90
António Macave <antonio.macave@gmail.com>
2025-06-02 19:58:03 +0200

    Removing Optional wrapper from findItemsByInvoiceId method

--- src/main/java/mz/co/faktorize/repository/ItemRepository.java
+++ src/main/java/mz/co/faktorize/repository/ItemRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+import mz.co.faktorize.dtos.ItemDto;
 import mz.co.faktorize.models.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Optional<List<Item>> findItemsByInvoiceId(Long invoiceId);
+    List<Item> findItemsByInvoiceId(Long invoiceId);
     Optional<Item> findItemById(Long itemId);
     void deleteItemById(Long id);
     void deleteItemsByInvoiceId(Long invoiceId);
