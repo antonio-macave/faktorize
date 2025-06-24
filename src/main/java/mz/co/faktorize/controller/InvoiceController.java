f8559cb502eb2319ad06126703d95afe9675a778
António Macave <antonio.macave@gmail.com>
2025-06-24 22:46:23 +0200

    Adding POST method

--- src/main/java/mz/co/faktorize/controller/InvoiceController.java
+++ src/main/java/mz/co/faktorize/controller/InvoiceController.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mz.co.faktorize.dtos.InvoiceDto;
 import mz.co.faktorize.models.Invoice;
 import mz.co.faktorize.service.InvoiceService;
 
@@ -23,4 +25,10 @@ public class InvoiceController {
     public ResponseEntity<Invoice> getInvoices(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(invoiceService.findInvoiceById(id));
     }
+
+    @PostMapping("/invoices")
+    public ResponseEntity<Invoice> saveInvoice(InvoiceDto invoice) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(invoiceService.saveInvoice(invoice));
+    }
+
 }
