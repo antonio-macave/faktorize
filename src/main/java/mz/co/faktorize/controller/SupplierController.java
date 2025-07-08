4f89146148be775e666b4fc474f41f56f0825b53
António Macave <antonio.macave@gmail.com>
2025-07-08 12:54:25 +0200

    Adding PUT method to update a Supplier object.

--- src/main/java/mz/co/faktorize/controller/SupplierController.java
+++ src/main/java/mz/co/faktorize/controller/SupplierController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,11 @@ public class SupplierController {
     @PostMapping("/suppliers")
     public ResponseEntity<SupplierDto> createSupplier(@RequestBody SupplierDto body) {
         return ResponseEntity.status(HttpStatus.CREATED).body(supplierService.saveSupplier(body));
+    }
+
+    @PutMapping("/suppliers/{id}")
+    public ResponseEntity<SupplierDto> updateSupplier(@PathVariable("id") Long id, @RequestBody SupplierDto body) {
+        return ResponseEntity.ok(supplierService.updateSupplier(id, body));
     }
 
     @GetMapping("/suppliers/{id}")
