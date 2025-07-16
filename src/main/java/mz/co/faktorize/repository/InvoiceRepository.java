c0f5ca6885d411a8ad7a9d88ed03d615a1db9879
António Macave <antonio.macave@gmail.com>
2025-07-16 09:01:53 +0200

    Removed unnecessary annotation (@Repository) and its import

--- src/main/java/mz/co/faktorize/repository/InvoiceRepository.java
+++ src/main/java/mz/co/faktorize/repository/InvoiceRepository.java
@@ -4,11 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import mz.co.faktorize.models.Invoice;
 
-@Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     Optional<Invoice> findInvoiceById(Long id);
     Optional<Invoice> findInvoiceByNumber(String name);
